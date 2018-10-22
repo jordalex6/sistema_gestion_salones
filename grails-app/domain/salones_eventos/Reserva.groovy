1094b46aa39b38b977c5a1a59bcbf1074c16aceb
Martin <42683251+polloeloy@users.noreply.github.com>
2018-10-22 18:54:19 -0300

    Update grails-app/domain/salones_eventos/Reserva.groovy
    actualizacion clase reserva

--- grails-app/domain/salones_eventos/Reserva.groovy
+++ grails-app/domain/salones_eventos/Reserva.groovy
@@ -2,21 +2,13 @@ package salones_eventos
 
 class Reserva {
 
-    Integer id_cliente
-    String nro_tarjeta
-    String tarjeta
-    String codigo_tarjeta
-    java.sql.Date fecha_actual
-    Date fecha
-    String horario_alquiler 
-    java.sql.Time hora
-   
-    BigDecimal montoint
-    BigDecimal numero_Salon
-    String tipo_Salon
-    int cantidad_Personas
-    String descripcion_Salon
-    BigDecimal precio
+    Cliente Cliente
+    Salon salon
+    Date fecha_actual
+    Date fecha_reserva
+     
+    float precio
+    
 
     static constraints = {
     }
