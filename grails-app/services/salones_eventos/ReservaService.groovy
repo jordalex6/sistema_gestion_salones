a4cfc138b8db1db14d5ca49e0f8881c5dec73ccb
Martin <42683251+polloeloy@users.noreply.github.com>
2018-11-05 18:02:26 -0300

    clave primaria reserva

---
+++ grails-app/services/salones_eventos/ReservaService.groovy
@@ -0,0 +1,18 @@
+package salones_eventos
+
+import grails.gorm.services.Service
+
+@Service(Reserva)
+interface ReservaService {
+
+    Reserva get(Serializable id)
+
+    List<Reserva> list(Map args)
+
+    Long count()
+
+    void delete(Serializable id)
+
+    Reserva save(Reserva reserva)
+
+}
